9117653e2aaebdb9d3afc7e65aca26bb05931eb6
Franco <donadiofranco7@gmail.com>
2026-01-27 19:08:59 -0600

    create user on build

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ cd ..
 
 export FLASK_APP=server.app
 flask db upgrade
+
+# Create admin user from env vars if set (skips if user already exists)
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_PASSWORD" ]; then
+  python -m server.cli create-user "$ADMIN_USERNAME" "$ADMIN_PASSWORD" || true
+fi
